583b5968e1f0ca3e52bec0e633a6cb3ff75ad493
Kyle Ratliff <KyleCRat@me.com>
2019-06-13 10:21:48 -0400

    Remove class method implementaiton

--- lib/ajax_reveal.rb
+++ lib/ajax_reveal.rb
@@ -3,14 +3,4 @@ require 'ajax_reveal/engine'
 
 module AjaxReveal
   class Error < StandardError; end
-  # Your code goes here...
-
-  def self.button(text, reveal_name, options = nil)
-    options ||= {}
-    options[:remote] = true
-
-    path = "/reveals/#{reveal_name}"
-
-    ActionController::Base.helpers.link_to(text, path, options)
-  end
 end
