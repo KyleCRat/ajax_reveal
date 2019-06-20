6a6b0a504551e918a25b5bd111bc3caaa8a4d5c3
Kyle Ratliff <KyleCRat@me.com>
2019-06-20 13:56:33 -0400

    Implement default_path and page_errors_for_missing_partials

--- app/controllers/ajax_reveal/ajax_reveals_controller.rb
+++ app/controllers/ajax_reveal/ajax_reveals_controller.rb
@@ -1,17 +1,23 @@
 module AjaxReveal
   class AjaxRevealsController < ApplicationController
-    # respond_to do |format|
-    #   format.js  { render 'new' }
-    #   format.all { render nothing: true, status: 200 } # for bots
-    # end
+    before_action :append_view_paths
 
     def show
       @reveal = CGI.unescape(params[:reveal])
-
+      p @reveal
       respond_to do |format|
         format.js  { render 'show' }
         format.all { render nothing: true, status: 200 }
       end
+    end
+
+    private
+
+    def append_view_paths
+      @default_path = AjaxReveal.configuration.default_path
+      return unless @default_path
+
+      append_view_path Rails.root.join('app', 'views', @default_path)
     end
   end
 end
---
+++ lib/errors/configuration.rb
@@ -0,0 +1,5 @@
+module AjaxReveal
+  module Errors
+    class Configuration < StandardError; end
+  end
+end
